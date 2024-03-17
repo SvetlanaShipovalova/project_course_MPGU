9e332752db8f80727e60ca89e2e2230ed5bcef3f
rweapon <mrkasper67@gmail.com>
2024-03-17 17:43:42 +0300

    Big css optimisation

--- project/index-mobile.js
+++ project/index-mobile.js
@@ -1,9 +1,13 @@
 window.onload = function () {
+    let viewport = document.querySelector('meta[name=viewport]');
+    let body = document.querySelector('body');
     if (navigator.userAgent.match(/Windows/i)){
-        window.location.assign("index.html");
+        // window.location.assign("index.html");
+        viewport.setAttribute('content', 'width=device-width, initial-scale=1');
+        body.classList.remove('device-mobile-optimized');
     }
-    document.querySelector('#PAGES-CONTAINER').style.top= "0px";
-    document.querySelector('#SITE-FOOTER').style.top= "0px";
-    document.querySelector('#SITE-HEADER').style.position= "relative";
+    // document.querySelector('#PAGES-CONTAINER').style.top= "0px";
+    // document.querySelector('#SITE-FOOTER').style.top= "0px";
+    // document.querySelector('#SITE-HEADER').style.position= "relative";
 
 }
